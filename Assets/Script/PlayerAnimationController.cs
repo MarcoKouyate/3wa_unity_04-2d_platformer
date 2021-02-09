9b4202dcf83e565be7760cad9b3eb54ece27a6af
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-09 19:06:59 +0100

    Player movement and animations

---
+++ Assets/Script/PlayerAnimationController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dwarf {
+    [RequireComponent(typeof(Animator))] 
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        [SerializeField] private float runningSpeed;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void IsGrounded(bool value)
+        {
+            _animator.SetBool("isGrounded", value);
+        }
+
+        public void Jump()
+        {
+            _animator.SetTrigger("jump");
+        }
+
+        public void Walk()
+        {
+            _animator.SetInteger("speed", 1);
+        }
+
+        public void Run()
+        {
+            _animator.SetInteger("speed", 2);
+        }
+
+        public void Stand()
+        {
+            _animator.SetInteger("speed", 0);
+        }
+
+        public void SetVerticalSpeed(float value)
+        {
+            _animator.SetFloat("verticalSpeed", value);
+        }
+
+        public void SetHorizontalSpeed(float value)
+        {
+            float speed = Mathf.Abs(value);
+
+            if (speed > runningSpeed)
+            {
+                Run();
+            } else if (speed > 0)
+            {
+                Walk();
+            } else
+            {
+                Stand();
+            }
+        }
+
+        private Animator _animator;
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dwarf {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private PlayerAnimationController _animation;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpHeight;
+        [SerializeField] private float _jumpInterval;
+        [SerializeField] LayerMask _groundLayers;
+        [SerializeField] Transform _groundCheck;
+
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _movement = Vector2.zero;
+            _isJumping = false;
+        }
+
+        private void Update()
+        {
+            bool isGrounded = IsGrounded();
+            _animation.IsGrounded(isGrounded);
+
+            if (isGrounded && Input.GetButtonDown("Jump"))
+            {
+                _animation.Jump();
+                _isJumping = true;
+                _jumpTime = Time.time + _jumpInterval;
+            }
+
+            if(_isJumping && Time.time > _jumpTime)
+            {
+                _isJumping = false;
+                _jump = _jumpHeight;
+            }
+
+
+
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            float inputMagnitude = Mathf.Clamp01(input.magnitude);
+            input.Normalize();
+            _movement = new Vector2(input.x * _speed, 0) * inputMagnitude;
+        }
+
+        private bool IsGrounded() {
+            return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayers);
+        }
+
+        private void FixedUpdate()
+        {
+            _movement.y = _rigidbody.velocity.y + _jump;
+            _jump = 0;
+            _rigidbody.velocity = _movement;
+
+            _animation.SetVerticalSpeed(_rigidbody.velocity.y);
+            _animation.SetHorizontalSpeed(_rigidbody.velocity.x);
+        }
+
+        private void Jump()
+        {
+
+            
+        }
+
+        private float _jumpTime;
+        private bool _isJumping;
+        private float _jump;
+        private Rigidbody2D _rigidbody;
+        private Vector2 _movement;
+    }
+}
