74b341c003231cbded0165896b4b25dc23edb0c6
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-11 15:57:43 +0100

    Camera Controller

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dwarf {
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float _smoothTime;
+        [SerializeField] private float _offset;
+
+        [Range(0.0f, 1f)]
+        [SerializeField] private float _threshold;
+
+        private void Awake()
+        {
+            _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            _transform = transform;
+            
+        }
+
+        private void Update()
+        {
+            Vector3 targetPosition = _playerTransform.position;
+            targetPosition.z = _transform.position.z;
+
+            float horizontalInput = Input.GetAxis("Horizontal") * _offset; 
+            if (Mathf.Abs(horizontalInput) >= _threshold)
+            {
+                Debug.Log("offset");
+                targetPosition.x += horizontalInput * _offset;
+                
+            }
+
+            _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _velocity, _smoothTime);
+
+
+        }
+
+        private Vector3 _velocity = Vector3.zero;
+        private Transform _playerTransform;
+        private Transform _transform;
+    }
+}
