9db842792270099803e495ecc9e6e8113f3bb8f6
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-12 15:06:38 +0100

    Better movement for mobile platforms

--- Assets/Script/MobilePlatform.cs
+++ Assets/Script/MobilePlatform.cs
@@ -4,36 +4,71 @@ namespace Dwarf {
     [RequireComponent(typeof(Rigidbody2D))]
     public class MobilePlatform : MonoBehaviour
     {
-        [SerializeField] private Transform _startTransform;
-        [SerializeField] private Transform _endTransform;
         [SerializeField] private float _speed;
         [SerializeField] private float _threshold;
+        [SerializeField] private Transform[] _targets;
 
 
         private void Awake()
         {
-            _rigidbody = GetComponent<Rigidbody2D>();
-            transform.position = _startTransform.position;
-        }
-
-        private void Update()
-        {
-            _targetPosition = (_changeDirection) ? _startTransform.position : _endTransform.position;
+            if (_targets.Length > 0)
+            {
+                transform.position = _targets[0].position;
+            }
         }
 
         private void FixedUpdate()
         {
-            Vector2 direction = (_targetPosition - (Vector2) transform.position);
+            if(_targets.Length > 0)
+            {
+                MovePlatform();
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+
+            for (int i = 0; i < _targets.Length - 1; i++)
+            {
+                Gizmos.DrawLine(_targets[i].position, _targets[i + 1].position);
+                Gizmos.DrawSphere(_targets[i].position, 0.1f);
+            }
+            
+            Gizmos.DrawSphere(transform.position, 0.2f);
+           
+            if(_targets.Length > 0)
+            {
+                Gizmos.DrawSphere(_targets[_targets.Length - 1].position, 0.1f);
+            }
+        }
+
+
+        private void MovePlatform()
+        {
+            Vector2 direction = (_targetPosition - (Vector2)transform.position);
             transform.position = (Vector2)transform.position + direction.normalized * Time.fixedDeltaTime * _speed;
 
             if (direction.sqrMagnitude <= _threshold * _threshold)
             {
-                _changeDirection = !_changeDirection;
+                _targetPosition = GetNextTarget().position;
             }
         }
 
+        private Transform GetNextTarget()
+        {
+            _index += (_changeDirection) ? -1 : +1;
+
+            if (_index <= 0 || _index >= _targets.Length - 1)
+            {
+                _changeDirection = !_changeDirection;
+            }
+
+            return _targets[_index];
+        }
+
         public bool _changeDirection;
-        private Rigidbody2D _rigidbody;
         private Vector2 _targetPosition;
+        private int _index;
     }
 }
