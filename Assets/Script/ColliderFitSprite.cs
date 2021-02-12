3d9a771683a29359b05b2434fc496e276474f41f
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-12 12:14:48 +0100

    Level design + mobile platform

---
+++ Assets/Script/ColliderFitSprite.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Dwarf {
+    [ExecuteInEditMode]
+    public class ColliderFitSprite : MonoBehaviour
+    {
+        void Awake()
+        {
+            runInEditMode = true;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _collider = GetComponent<BoxCollider2D>();
+        }
+
+        void Update()
+        { 
+            _collider.offset = new Vector2(0, 0);
+            _collider.size = new Vector3(_spriteRenderer.bounds.size.x / transform.lossyScale.x,
+                                         _spriteRenderer.bounds.size.y / transform.lossyScale.y,
+                                         _spriteRenderer.bounds.size.z / transform.lossyScale.z);
+        }
+
+        private SpriteRenderer _spriteRenderer;
+        private BoxCollider2D _collider;
+    }
+}
---
+++ Assets/Script/MobilePlatform.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Dwarf {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class MobilePlatform : MonoBehaviour
+    {
+        [SerializeField] private Transform _startTransform;
+        [SerializeField] private Transform _endTransform;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _threshold;
+
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            transform.position = _startTransform.position;
+        }
+
+        private void Update()
+        {
+            _targetPosition = (_changeDirection) ? _startTransform.position : _endTransform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 direction = (_targetPosition - (Vector2)_rigidbody.position);
+            Vector2 newPosition = _rigidbody.position + direction.normalized * Time.fixedDeltaTime * _speed;
+            _rigidbody.MovePosition(newPosition);
+
+            if (direction.sqrMagnitude <= _threshold * _threshold)
+            {
+                _changeDirection = !_changeDirection;
+            }
+        }
+
+        public bool _changeDirection;
+        private Rigidbody2D _rigidbody;
+        private Vector2 _targetPosition;
+    }
+}
