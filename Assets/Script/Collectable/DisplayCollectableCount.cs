32df9a71b8584dd7592ee080563959b0884f2e05
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-15 15:37:55 +0100

    Display collectable count on screen

---
+++ Assets/Script/Collectable/DisplayCollectableCount.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Dwarf {
+    [RequireComponent(typeof(TMP_Text))]
+    public class DisplayCollectableCount : MonoBehaviour
+    {
+        [SerializeField] private IntVariable _collectable;
+
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            _text.text = $"{_collectable.count}";
+        }
+
+        private TMP_Text _text;
+    }
+}
