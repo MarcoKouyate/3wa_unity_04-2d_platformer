805fb9ed60c111e4490aa67006dd600ca99b5d57
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-15 11:59:29 +0100

    Rspawn player when losing

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 namespace Dwarf {
     public class LevelManager : MonoBehaviour
     {
+        private void Start()
+        {
+            _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            _startPosition = _playerTransform.position;
+        }
+
         public void Lose()
         {
-            Debug.Log("Lose");
+            _playerTransform.position = _startPosition;
         }
 
         public void Win()
         {
             Debug.Log("Win");
         }
+
+        private Transform _playerTransform;
+        private Vector3 _startPosition;
     }
 }
